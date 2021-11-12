54af98007b0cfdaf79df6bbda187b0e2ef86fce4
AbuSohan <sohan@inflexionpointbd.com>
2021-11-12 22:38:58 +0600

    Fletch multiple city weather through database

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
+import requests
+from .models import City
 
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=00c684d3fef3d6d4412193d222573b1c'
 
-    # returns the index.html template
-    return render(request, 'weather/index.html')
+    city = 'Dhaka'
+
+    city_weather = requests.get(url.format(city)).json()  # request the API data and convert the JSON to Python data
+    # types
+    # print(city_weather)
+
+    weather = {
+        'city': city,
+        'temperature': city_weather['main']['temp'],
+        'description': city_weather['weather'][0]['description'],
+        'icon': city_weather['weather'][0]['icon']
+    }
+
+    # context = {'weather': weather}
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(
+            url.format(city)).json()  # request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)  # add the data for the current city into our list
+
+    context = {'weather_data': weather_data}
+
+    return render(request, 'weather/index.html', context)  # returns the index.html template
